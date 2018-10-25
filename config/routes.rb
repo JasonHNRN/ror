b5c1be83ef307bf607bfb9d77b412e20a6c55e58
i320418 <jason.li05@sap.com>
2018-10-25 12:20:25 +0800

    initial added

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  root 'welcome#index'
+
+  get 'welcome/index'
+  get 'welcome/next'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #resource 'users'  # 这种和下面这种写法都是可行的。
+  resource :users
+  resource :sessions
+
+end
